f8a1b1284e498213e135b8e2de1c4918a7a4fe39
Zahid Hasan <zhdruvo@gmail.com>
2022-04-16 09:52:15 +0600

    Initial commit

---
+++ Dextar/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Dextar.Startup))]
+namespace Dextar
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
